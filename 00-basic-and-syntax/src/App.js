d5795950c28573bd82cee4b9941d8d2c35e3af06
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2018-08-30 00:04:09 +0700

    progress on basics

--- 00-basic-and-syntax/src/App.js
+++ 00-basic-and-syntax/src/App.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons:[
+      { name: 'archie', age: 22 },
+      { name: 'atikha', age: 23 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hi, im a react app.</h1>
+        <button>switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>Hola Amigos</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hello Friends</Person>
       </div>
     );
+    // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Im react app'))
   }
 }
 
