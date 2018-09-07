3c2902fdf975d31ae1a2ad066d4d831429257dd8
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2018-09-07 19:14:59 +0700

    update burger builder

---
+++ 03-http/src/containers/Blog/Blog.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import './Blog.css';
+import axios from '../../axios';
+
+class Blog extends Component {
+    state = {
+        posts: [],
+        selectedPostId: null
+    }
+
+    // mounted
+    async componentDidMount(){
+        const { data: posts } = await axios.get('/posts');
+        const updatedPosts = posts.slice(0, 4).map(post => ({
+            ...post,
+            author: 'ikhaa'
+        }));
+        this.setState({ posts: updatedPosts });
+    }
+
+    selectPostHandler(id){
+        this.setState({ selectedPostId: id });
+    }
+
+    render () {
+        const posts = this.state.posts.map(post => (
+            <Post 
+                key={post.id}
+                title={post.title}
+                author={post.author}
+                clicked={() => this.selectPostHandler(post.id)}
+            />
+        ));
+
+        return (
+            <div>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <section>
+                    <FullPost id={this.state.selectedPostId} />
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog;
