df488082ddfa4edda0cf337fc99833e48f167e75
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2018-10-25 18:16:58 +0700

    update burger builder

--- 02-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ 02-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -37,7 +37,11 @@ class BurgerBuilder extends Component {
     }
 
     purchaseHandler = () => {
-        this.setState( { purchasing: true } );
+        if (this.props.isAuthenticated){
+            this.setState({ purchasing: true });
+        } else {
+            this.props.history.push('/auth');
+        }
     }
 
     purchaseCancelHandler = () => {
@@ -64,6 +68,7 @@ class BurgerBuilder extends Component {
                 <Aux>
                     <Burger ingredients={this.props.ings} />
                     <BuildControls
+                        isAuth={this.props.isAuthenticated}
                         ingredientAdded={this.props.onIngredientAdded}
                         ingredientRemoved={this.props.onIngredientRemoved}
                         disabled={disabledInfo}
@@ -94,7 +99,8 @@ const mapStateToProps = state => {
     return {
         ings: state.burgerBuilder.ingredients,
         price: state.burgerBuilder.totalPrice,
-        error: state.burgerBuilder.error
+        error: state.burgerBuilder.error,
+        isAuthenticated: state.auth.token
     };
 }
 
--- 02-burger-builder/src/store/actions/index.js
+++ 02-burger-builder/src/store/actions/index.js
@@ -9,5 +9,5 @@ export {
     fetchOrders
 } from './order';
 export {
-    auth
+    auth, logout
 } from './auth';
