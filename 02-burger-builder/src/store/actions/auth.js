c12c22d9376d12bbb33c3b00be5932737cf4cc87
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2018-10-31 13:22:44 +0700

    update

--- 02-burger-builder/src/store/actions/auth.js
+++ 02-burger-builder/src/store/actions/auth.js
@@ -48,9 +48,9 @@ export const auth = (email, password, isSignup) => {
             password: password,
             returnSecureToken: true
         };
-        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyB5cHT6x62tTe-g27vBDIqWcwQWBSj3uiY';
+        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDh-RybjpgnDTfKGyzWZwe_4wpOhx6BbKA';
         if (!isSignup) {
-            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyB5cHT6x62tTe-g27vBDIqWcwQWBSj3uiY';
+            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDh-RybjpgnDTfKGyzWZwe_4wpOhx6BbKA';
         }
         axios.post(url, authData)
             .then(response => {
