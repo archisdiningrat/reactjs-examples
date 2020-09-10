dbd879f78c65bc630b430ea57b312912443506e5
Archie <archie.isdiningrat@gmail.com>
2020-09-10 21:01:37 +0700

    update

--- 06-hooks/src/components/Ingredients/Ingredients.js
+++ 06-hooks/src/components/Ingredients/Ingredients.js
@@ -2,11 +2,15 @@ import React, { useState, useEffect, useCallback } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
+import ErrorModal from "../UI/ErrorModal";
 import Search from "./Search";
 
 function Ingredients() {
   const [ingredientProp, setIngredient] = useState([]);
+  const [isLoadingProp, setLoading] = useState(false);
+  const [hasErrorProp, setHasError] = useState(null);
 
+  // wrap with useCallback to avoid handler being stuck in infinite loop
   const loadIngredientsHandler = useCallback((ingredients) => {
     setIngredient(ingredients);
   }, []);
@@ -31,6 +35,8 @@ function Ingredients() {
   }, [ingredientProp]); // will act like component did update
 
   const addIngredientHandler = (ingredient) => {
+    setLoading(true);
+
     // post data to firebase
     fetch("https://my-portfolio-c4789.firebaseio.com/ingredients.json", {
       method: "POST",
@@ -42,18 +48,41 @@ function Ingredients() {
         setIngredient((prev) => {
           return [...prev, { id: data.name, ...ingredient }];
         });
+      })
+      .catch((err) => {
+        setHasError(err.message);
       });
+  };
+
+  const removeIngredienthandler = (id) => {
+    setLoading(true);
+    fetch(`https://my-portfolio-c4789.firebaseio.com/ingredients/${id}.json`, {
+      method: "DELETE",
+    }).then(() => {
+      setIngredient((prev) => {
+        return prev.filter((i) => i.id !== id);
+      });
+      setLoading(false);
+    });
+  };
+
+  const clearErrorHandler = () => {
+    setHasError(null);
+    setLoading(false);
   };
 
   return (
     <div className="App">
-      <IngredientForm onAdd={addIngredientHandler} />
+      {hasErrorProp && (
+        <ErrorModal onClose={clearErrorHandler}>{hasErrorProp}</ErrorModal>
+      )}
+      <IngredientForm onAdd={addIngredientHandler} loading={isLoadingProp} />
 
       <section>
         <Search onLoadIngredients={loadIngredientsHandler} />
         <IngredientList
           ingredients={ingredientProp}
-          onRemoveItem={() => {}}
+          onRemoveItem={removeIngredienthandler}
         ></IngredientList>
       </section>
     </div>
--- 06-hooks/src/components/Ingredients/Search.js
+++ 06-hooks/src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 import Card from "../UI/Card";
 import "./Search.css";
@@ -6,25 +6,38 @@ import "./Search.css";
 const Search = React.memo((props) => {
   const { onLoadIngredients } = props;
   const [searchProp, setSearch] = useState("");
+  const searchRef = useRef();
 
   useEffect(() => {
-    const query = searchProp.length
-      ? `?orderBy="title"&equalTo="${searchProp}"`
-      : "";
+    // act as deboucer
+    const timer = setTimeout(() => {
+      if (searchProp === searchRef.current.value) {
+        const query = searchProp.length
+          ? `?orderBy="title"&equalTo="${searchProp}"`
+          : "";
 
-    fetch("https://my-portfolio-c4789.firebaseio.com/ingredients.json" + query)
-      .then((reponse) => reponse.json())
-      .then((data) => {
-        const loadedIngredients = [];
-        for (const key in data) {
-          loadedIngredients.push({
-            id: key,
-            ...data[key],
+        fetch(
+          "https://my-portfolio-c4789.firebaseio.com/ingredients.json" + query
+        )
+          .then((reponse) => reponse.json())
+          .then((data) => {
+            const loadedIngredients = [];
+            for (const key in data) {
+              loadedIngredients.push({
+                id: key,
+                ...data[key],
+              });
+            }
+            onLoadIngredients(loadedIngredients);
           });
-        }
-        onLoadIngredients(loadedIngredients);
-      });
-  }, [onLoadIngredients, searchProp]);
+      }
+    }, 500);
+
+    // remove overlapping timer
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [onLoadIngredients, searchProp, searchRef]);
 
   return (
     <section className="search">
@@ -32,6 +45,7 @@ const Search = React.memo((props) => {
         <div className="search-input">
           <label>Filter by Title</label>
           <input
+            ref={searchRef}
             type="text"
             value={searchProp}
             onChange={(event) => setSearch(event.target.value)}
