a18c79d8837e4d08513f7ac0d01cdd87cd4a73bb
Archie <archie.isdiningrat@gmail.com>
2020-09-10 18:15:43 +0700

    add simple use effect

--- 06-hooks/src/components/Ingredients/Ingredients.js
+++ 06-hooks/src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
@@ -7,10 +7,35 @@ import Search from "./Search";
 function Ingredients() {
   const [ingredientProp, setIngredient] = useState([]);
 
+  // run every render cycle
+  useEffect(() => {
+    fetch("https://my-portfolio-c4789.firebaseio.com/ingredients.json")
+      .then((reponse) => reponse.json())
+      .then((data) => {
+        const loadedIngredients = [];
+        for (const key in data) {
+          loadedIngredients.push({
+            id: key,
+            ...data[key],
+          });
+        }
+        setIngredient(loadedIngredients);
+      });
+  }, []); // no dependency, will act like component did mount
+
   const addIngredientHandler = (ingredient) => {
-    setIngredient((prev) => {
-      return [...prev, { id: Math.random().toString(), ...ingredient }];
-    });
+    // post data to firebase
+    fetch("https://my-portfolio-c4789.firebaseio.com/ingredients.json", {
+      method: "POST",
+      body: JSON.stringify(ingredient),
+      headers: { "Content-type": "application/json" },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setIngredient((prev) => {
+          return [...prev, { id: data.name, ...ingredient }];
+        });
+      });
   };
 
   return (
